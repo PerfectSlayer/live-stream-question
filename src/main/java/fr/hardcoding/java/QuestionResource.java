ef0f398dd6d20a564d1bdfe6f088000ce43e9945
PerfectSlayer <bruce.bujon@gmail.com>
2020-05-22 11:20:23 +0200

    feat: Add question feature

--- src/main/java/fr/hardcoding/java/QuestionResource.java
+++ src/main/java/fr/hardcoding/java/QuestionResource.java
@@ -1,66 +1,83 @@
 package fr.hardcoding.java;
 
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import java.util.ArrayList;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * {@link Question} REST endpoint.
  *
  * @author Bruce BUJON (bruce.bujon(at)gmail(dot)com)
  */
+@ApplicationScoped
 @Path("/questions")
+@Produces(APPLICATION_JSON)
 public class QuestionResource {
 
     private final Map<UUID, Question> questions;
 
     public QuestionResource() {
         this.questions = new HashMap<>();
+        addSampleData();
     }
 
-    @GET
-    @Produces(APPLICATION_JSON)
-    public List<Question> hello() {
-
-        List<Question> questions = new ArrayList<>();
-
-        questions.add(new Question(
+    private void addSampleData() {
+        Question question = new Question(
                 UUID.randomUUID(),
                 "Bruce",
                 "Est-ce que ça marche ?",
                 "https://avatars3.githubusercontent.com/u/1766222?s=460&u=1567973b4b7166af57d0c534a51ad6b5922aa0e9&v=4"
-        ));
-
-        questions.add(new Question(
-                        UUID.randomUUID(),
-                        "Charles",
-                        "Juste une question avec un texte assez long pour tester la taille de box parce qu'il y a de très grande chance que ce cas existe.",
-                        "/img/question.png"
-                )
         );
-
-        return questions;
+        this.questions.put(question.uuid, question);
+        question = new Question(
+                UUID.randomUUID(),
+                "Charles",
+                "Juste une question avec un texte assez long pour tester la taille de box parce qu'il y a de très grande chance que ce cas existe.",
+                "/img/question.png"
+        );
+        this.questions.put(question.uuid, question);
     }
 
+    @GET
+    public Collection<Question> hello() {
+        return this.questions.values();
+    }
 
     @POST
     @Consumes(APPLICATION_JSON)
-    public void addQuestion(Question question) {
-        UUID uuid = UUID.randomUUID();
-        question.uuid = uuid;
-        question.profileUrl = "/img/question.png";
-        this.questions.put(uuid, question);
+    public Question addQuestion(Question data) {
+        Question question = new Question(
+                UUID.randomUUID(),
+                data.userName,
+                data.text,
+                "/img/question.png"
+        );
+        this.questions.put(question.uuid, question);
+        return question;
     }
 
-
+    @DELETE
+    @Path("/{id}")
+    public Response removeQuestion(@PathParam(value = "id") String id) {
+        UUID uuid = UUID.fromString(id);
+        if (this.questions.remove(uuid) != null) {
+            return Response.ok().build();
+        } else {
+            return Response.status(NOT_FOUND).build();
+        }
+    }
 }
--- src/main/resources/META-INF/resources/js/questions.js
+++ src/main/resources/META-INF/resources/js/questions.js
@@ -1,10 +1,10 @@
 const questionListDiv = $('#questionList');
 
-
 function loadQuestions() {
-    fetch("questions")
+    fetch("/questions")
         .then(response => response.json())
         .then(questions => {
+            questionListDiv.empty();
             for (const question of questions) {
                 questionListDiv.append(createQuestionElement(question));
             }
@@ -15,12 +15,45 @@ function createQuestionElement(question) {
     return $("<li class=\"list-group-item question\">" +
         "    <img src=\"" + question.profileUrl + "\">" +
         "    <div class=\"questionActions\">" +
-        "         <i class=\"far fa-bookmark fa-lg\"></i>" +
-        "         <i class=\"fas fa-trash-alt fa-lg\"></i>" +
+        "         <a  href='#' onclick='promote(\"" + question.uuid + "\")'><i class=\"far fa-bookmark fa-lg\"></i></a>" +
+        "         <a href='#' onclick='removeQuestion(\"" + question.uuid + "\")'><i class=\"fas fa-trash-alt fa-lg\"></i></a>" +
         "    </div>" +
         "    <div class=\"questionUsername\">" + question.userName + "</div>" +
         "    <div class=\"questionText\">" + question.text + "</div>" +
         "</li>")
 }
+
+function addQuestion() {
+    fetch("/questions", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "userName": $('#questionUsername').val(),
+            "text": $('#questionText').val()
+        }),
+    }).then(_ => loadQuestions());
+}
+
+function removeQuestion(questionId) {
+    fetch("/questions/" + questionId, {
+        method: "DELETE"
+    }).then(_ => loadQuestions());
+}
+
+function promote(questionId) {
+
+}
+
+function demote() {
+
+}
+
+const addQuestionForm = $('#addQuestionForm');
+addQuestionForm.submit(function (event) {
+    addQuestion();
+    event.preventDefault();
+});
 
 loadQuestions();
